99c7e281e17b6bbf35defb387d160c115483ddd3
Jon-Mikkel Korsvik <48263282+jkorsvik@users.noreply.github.com>
2024-02-16 13:23:41 +0000

    reqs and install script

---
+++ install_reqs.sh
@@ -0,0 +1,8 @@
+# Create Python 3 virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
